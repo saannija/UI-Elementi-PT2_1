1133d6591acdc8e8a6e508c45780b15c6f1adc51
Bugsters104 <bugsters104@lvt.lv>
2023-04-03 15:10:06 +0300

    Pabeigts darbs ar Toggle elementiem

--- Assets/Skripti/AtteluParadisana.cs
+++ Assets/Skripti/AtteluParadisana.cs
@@ -2,17 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour {
 	public GameObject bins;
 	public GameObject lacitis;
 	public GameObject tante;
 	public GameObject masina;
+    public GameObject paKreisi;
+    public GameObject paLabi;
 
 	public void BinaAttels(bool vertiba)
 	{
 		bins.SetActive(vertiba);
-	}
+        paKreisi.GetComponent<Toggle>().interactable = vertiba;
+        paLabi.GetComponent<Toggle>().interactable = vertiba;
+    }
 
 	public void LacisaAttels(bool vertiba)
 	{
@@ -28,4 +33,14 @@ public class AtteluParadisana : MonoBehaviour {
 	{
 		masina.SetActive(vertiba);
 	}
+
+    public void PaKreisiBins()
+    {
+        bins.transform.localScale = new Vector2(1, 1);
+    }
+
+    public void PaLabiBins()
+    {
+        bins.transform.localScale = new Vector2(-1, 1);
+    }
 }
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Projekts, kas demonstrē UI elementu lietojumu, tēma: MR. Bean
 - [x] C# skripta izveide un piesaiste objektam
 - [x] Image elementa lietojums
 - [x] Toggle elementa lietojums
-- [ ] Toggle Group komponentes piesaiste
+- [x] Toggle Group komponentes piesaiste
 - [ ] Drop down elementa lietojums
 - [ ] Slider elementa lietojums
 - [ ] Audio source objekta lietojums
